2ac1ec1cfab6fbc69a409970033a3d341ec2ccbc
Chris Kincanon <chris@chris-laptop.(none)>
2010-05-16 18:42:15 -0700

    finalized layout and twitter feed with contact info

--- lib/rss_reader.rb
+++ lib/rss_reader.rb
@@ -3,23 +3,28 @@ require 'rss/2.0'
 require 'open-uri'
    
 class RssReader
-  attr_accessor :title, :link, :description, :date, :items, :source
+  attr_accessor :title, :link, :description, :date, :items, :source, :status
 
-  def initialize(source)
+  def initialize(source, limit=0)
       @source = source
-      self.consume() unless source.nil?
+      self.consume(limit) unless source.nil?
   end
 
-  def consume(options={})
-     content=""
-     open(self.source){|s| content = s.read}
-     rss = RSS::Parser.parse(content, false)
-     
-     self.title = rss.channel.title
-     self.link = rss.channel.link
-     self.description = rss.channel.description
-     self.date = rss.channel.date
-     self.items = rss.items
+  def consume(limit=0,options={})
+     begin
+		   content=""
+		   open(self.source){|s| content = s.read}
+		   rss = RSS::Parser.parse(content, false)
+		   
+		   self.title = rss.channel.title
+		   self.link = rss.channel.link
+		   self.description = rss.channel.description
+		   self.date = rss.channel.date
+		   self.items = limit == 0 ? rss.items : rss.items[0..(limit-1)]
+       self.status = true
+     rescue
+       self.status = false
+     end
   end
   
   def linkify_items()
